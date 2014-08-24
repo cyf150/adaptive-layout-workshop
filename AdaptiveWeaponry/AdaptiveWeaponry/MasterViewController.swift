73fa608201c12d5e5fe06c15ded1680b170406e3
Sam Davies <sdavies@shinobicontrols.com>
2014-08-23 21:57:47 -0600

    Removing boilerplate

--- AdaptiveWeaponry/AdaptiveWeaponry/MasterViewController.swift
+++ AdaptiveWeaponry/AdaptiveWeaponry/MasterViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class MasterViewController: UITableViewController {
 
   var detailViewController: DetailViewController? = nil
-  var objects = NSMutableArray()
-
 
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -25,26 +23,8 @@ class MasterViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-    let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-    self.navigationItem.rightBarButtonItem = addButton
-    if let split = self.splitViewController {
-        let controllers = split.viewControllers
-        self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
-    }
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  func insertNewObject(sender: AnyObject) {
-    objects.insertObject(NSDate.date(), atIndex: 0)
-    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-  }
 
   // MARK: - Segues
 
@@ -76,21 +56,6 @@ class MasterViewController: UITableViewController {
     cell.textLabel.text = object.description
     return cell
   }
-
-  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-  }
-
-  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-        objects.removeObjectAtIndex(indexPath.row)
-        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-  }
-
 
 }
 
